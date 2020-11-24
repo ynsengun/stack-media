5e9281f30b8e3d84906124caad613d48c2c3970b
talhasen123 <taklamaka1919@gmail.com>
2020-11-25 00:19:38 +0300

    MySQL connection

--- server/src/Server.ts
+++ server/src/Server.ts
@@ -1,9 +1,30 @@
+import { connected } from "process";
 import {config} from "./Config";
 import socket from "./Socket";
 
 function startServer(){
     const PORT: number = <number>config.PORT;
     socket.listen(PORT); // Server is on and socket listens the events
+    connectDB();
 }
 
+function connectDB() {
+    var mysql = require('mysql');
+
+    var con = mysql.createConnection({
+        host: "dijkstra.ug.bcc.bilkent.edu.tr",
+        database: "hakan_sivuk",
+        user: "hakan.sivuk",
+        password: "k3iLQqwg",
+        port: 3306
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        con.query("SELECT * FROM User", function (err, result, fields) {
+          if (err) throw err;
+          console.log(result);
+        });
+    });
+}
 startServer();
