d79c19e6bab8ce8bcd320eb1df0c75ec5fc37118
Yusuf Nevzat Sengun <yusuf.nevzat.sengun@gmail.com>
2020-12-16 18:52:41 +0300

    friend bar

--- client/src/components/MainPage/FriendsBar.js
+++ client/src/components/MainPage/FriendsBar.js
@@ -8,6 +8,40 @@ import "../../css/MainPage/FriendsBar.css";
 import FriendLabel from "./FriendLabel";
 
 export default function FriendsBar() {
+  const [friends, setFriends] = useState([{}]);
+  const [textInput, setTextInput] = useState("");
+
+  const handleChange = (e) => {
+    const { value, name } = e.currentTarget;
+    setTextInput(value);
+  };
+
+  useEffect(() => {
+    // TODO fetch friends with their last watch
+
+    setFriends([
+      {
+        name: "friend1",
+        lastWatch: "watch1",
+      },
+      {
+        name: "friend2",
+        lastWatch: "watch1",
+      },
+      {
+        name: "friend3",
+        lastWatch: "watch1",
+      },
+    ]);
+  }, []);
+
+  const handleNewFriend = () => {
+    // TODO fetch add new friend, if valid then fetch its last watch
+
+    setFriends([...friends, { name: textInput, lastWatch: "aa" }]);
+    setTextInput(""); // to reset the input text field
+  };
+
   return (
     <div className="FriendsBar">
       <div className="MediaSection">
@@ -15,19 +49,25 @@ export default function FriendsBar() {
           <h3 className="Header3">Friends</h3>
         </div>
         <div className="FriendsScroolBar">
-          <FriendLabel friendName={"Cevat"} movieName={"CS353"}></FriendLabel>
-          <FriendLabel friendName={"Cevat"} movieName={"CS353"}></FriendLabel>
-          <FriendLabel friendName={"Cevat"} movieName={"CS353"}></FriendLabel>
-          <FriendLabel friendName={"Cevat"} movieName={"CS353"}></FriendLabel>
-          <FriendLabel friendName={"Cevat"} movieName={"CS353"}></FriendLabel>
-          <FriendLabel friendName={"Cevat"} movieName={"CS353"}></FriendLabel>
-          <FriendLabel friendName={"Cevat"} movieName={"CS353"}></FriendLabel>
-          <FriendLabel friendName={"Cevat"} movieName={"CS353"}></FriendLabel>
+          {friends.map((friend) => (
+            <FriendLabel
+              friendName={friend.name}
+              movieName={friend.lastWatch}
+            ></FriendLabel>
+          ))}
         </div>
 
         <div className="MediaChoiceContainer">
-          <input type="text" style={{ width: 150 }}></input>
-          <button>Add Friend</button>
+          <input
+            className="w-75 mx-auto"
+            type="text"
+            name="friendText"
+            value={textInput}
+            onChange={handleChange}
+          />
+          <button onClick={handleNewFriend} className="w-75 mx-auto mt-1">
+            Add Friend
+          </button>
         </div>
       </div>
     </div>
