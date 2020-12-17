da9868f4b2c5b549399033a4b68075d1acddcdc5
cevataykans <aykansevinc66@gmail.com>
2020-12-17 12:30:55 +0300

    Friend page fetch implementations.

--- client/src/components/MainPage/FriendsBar.js
+++ client/src/components/MainPage/FriendsBar.js
@@ -6,6 +6,8 @@ import "../../css/MainPage/Home.css";
 import "../../css/MainPage/FriendsBar.css";
 
 import FriendLabel from "./FriendLabel";
+import { checkResponse } from "../../util/ResponseUtil";
+import { getAuthName, getAuthToken } from "../../util/AuthenticationUtil";
 
 export default function FriendsBar() {
   const [friends, setFriends] = useState([{}]);
@@ -17,7 +19,28 @@ export default function FriendsBar() {
   };
 
   useEffect(() => {
-    // TODO fetch friends with their last watch
+    // TODO fetch friends of the user with their last watch => wait server
+    // fetch("http://localhost:4000/api/user/getFriendActivities", {
+    //   method: "POST",
+    //   mode: "cors",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     token: getAuthToken(),
+    //     username: getAuthName(),
+    //   }),
+    // })
+    //   .then((r) => checkResponse(r))
+    //   .then((r) => r.json())
+    //   .then((r) => {
+    //     let resArray = r.data;
+    //     console.log( resArray);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     toast.error("Error, could not load your friends!");
+    //   });
 
     setFriends([
       {
@@ -36,20 +59,67 @@ export default function FriendsBar() {
   }, []);
 
   const handleNewFriend = () => {
-    // TODO fetch add new friend, if valid then fetch its last watch or return it as the result of first fetch
+    // fetch add new friend
     console.log( "Add new friend!");
-    setFriends([...friends, { name: textInput, lastWatch: "aa" }]);
-    setTextInput(""); // to reset the input text field
+    fetch("http://localhost:4000/api/user/sendFriendshipInvitation", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: getAuthToken(),
+        username: getAuthName(),
+
+        invitedUsername: textInput,
+      }),
+    })
+      .then((r) => checkResponse(r))
+      .then((r) => r.json())
+      .then((r) => {
+        toast.success( "Friendship request is sent!");
+        setTextInput(""); // to reset the input text field
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Error, could not send friendship request!");
+      });
+
+    // setFriends([...friends, { name: textInput, lastWatch: "aa" }]);
+    // setTextInput(""); // to reset the input text field
   };
 
   const handleDeleteFriend = (friendName) => {
-    //TODO fetch delete friend
-
+    
     let temp = [];
     friends.forEach((friend) => {
       if (friendName != friend.name) temp.push(friend);
     });
-    setFriends(temp);
+    
+    //TODO fetch delete friend => wait for server to implement getFriendActivities to actually test it
+    fetch("http://localhost:4000/api/user/removeFriend", {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        token: getAuthToken(),
+        username: getAuthName(),
+
+        friendUsername: friendName,
+      }),
+    })
+      .then((r) => checkResponse(r))
+      .then((r) => r.json())
+      .then((r) => {
+        toast.success( "Friend " + friendName + " is successfully removed!");
+        setFriends(temp);
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Error, could not remove friend " + friendName + "!");
+      });
   };
 
   return (
