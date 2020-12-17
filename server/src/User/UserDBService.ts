9042e475587c56223783d3a871013e40564a3f29
hakansivuk <hakansivuk@gmail.com>
2020-12-17 22:11:39 +0300

    Fix time format

--- server/src/User/UserDBService.ts
+++ server/src/User/UserDBService.ts
@@ -143,9 +143,14 @@ export class UserDBService {
         let resultActivities = null;
 
         let sqlQuery = "SELECT UserFriend.username, TIMESTAMP(MAX(WatchMedia.timeStamp)) as lastActivity, WatchMedia.name as mediaName, WatchMedia.mediaId FROM ((SELECT friend1Username as username FROM Friendship WHERE friend2Username = '" + user.username + "') UNION (SELECT friend2Username as username FROM Friendship WHERE friend1Username = '" + user.username + "')) UserFriend LEFT OUTER JOIN (SELECT Watch.*, Media.name FROM Watch INNER JOIN Media ON Media.mediaId=Watch.mediaId ) WatchMedia ON UserFriend.username=WatchMedia.username GROUP BY UserFriend.username ORDER BY WatchMedia.timeStamp DESC;";
-        
+        //SELECT UserFriend.username, TIMESTAMP(MAX(WatchMedia.timeStamp)) as lastActivity, WatchMedia.name as mediaName, WatchMedia.mediaId FROM ((SELECT friend1Username as username FROM Friendship WHERE friend2Username = 'yusuf') UNION (SELECT friend2Username as username FROM Friendship WHERE friend1Username = 'yusuf')) UserFriend LEFT OUTER JOIN (SELECT Watch.*, Media.name FROM Watch INNER JOIN Media ON Media.mediaId=Watch.mediaId ) WatchMedia ON UserFriend.username=WatchMedia.username GROUP BY UserFriend.username ORDER BY WatchMedia.timeStamp DESC;
         try {
             resultActivities = await this.db.sendQuery(sqlQuery);
+            console.log(resultActivities[0]);
+            for(let i = 0 ; i < resultActivities.length ; i++){
+                let formattedTime = resultActivities[i].lastActivity.toLocaleString();
+                resultActivities[i].lastActivity = formattedTime;
+            }
             /*sqlQuery = "SELECT F.friend1Username, WHERE F.friend2Username = '" + user.username + "';";
              resultFriends.concat(await this.db.sendQuery(sqlQuery));
             if (resultFriends.length > 0)
